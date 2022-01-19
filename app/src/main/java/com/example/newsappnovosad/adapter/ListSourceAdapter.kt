0c2614a638c152b31b1e64f17390e750e263b783
Anton Novosad <noobito.2011@gmail.com>
2022-01-19 17:41:26 +0300

    added DIFF_CALLBACK; change methods onCreateViewHolder and onBlindVewHolder

--- app/src/main/java/com/example/newsappnovosad/adapter/ListSourceAdapter.kt
+++ app/src/main/java/com/example/newsappnovosad/adapter/ListSourceAdapter.kt
@@ -3,35 +3,39 @@ package com.example.newsappnovosad.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.example.newsappnovosad.R
-import com.example.newsappnovosad.model.WebSite
-import com.example.newsappnovosad.service.ItemClickListener
+import com.example.newsappnovosad.model.Article
 
-class ListSourceAdapter(private val context: Context, private val webSite: WebSite) :
-    RecyclerView.Adapter<ListSourceViewHolder>() {
+class ListSourceAdapter(
+    private val context: Context
+) : ListAdapter<Article, ListSourceViewHolder>(DIFF_CALLBACK) {
+
     @SuppressLint("ResourceType")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSourceViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val itemView = inflater.inflate(R.id.id_News, parent, false)
+        val itemView = inflater.inflate(R.layout.news, parent, false)
         return ListSourceViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        return webSite.articles!!.size
+    override fun onBindViewHolder(holder: ListSourceViewHolder, position: Int) {
+        holder.bind(getItem(position)) {
+            Toast.makeText(context, "Bind", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+private object DIFF_CALLBACK : DiffUtil.ItemCallback<Article>() {
+
+    override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+        return oldItem.url == newItem.url
     }
 
-    override fun onBindViewHolder(holder: ListSourceViewHolder, position: Int) {
-        holder.source_title.tooltipText = webSite.articles!![position].source!!.name
-
-        holder.setItemClickListener(object : ItemClickListener
-        {
-            override fun onCLick(view: View, position: Int) {
-                Toast.makeText(context, "pampam", Toast.LENGTH_SHORT).show()
-            }
-        })
+    @SuppressLint("DiffUtilEquals")
+    override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+        return oldItem == newItem
     }
 }
