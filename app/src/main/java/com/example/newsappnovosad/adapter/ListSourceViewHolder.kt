f7ecf1b4cac42fca39bedd377bdfddec61884404
Anton Novosad <noobito.2011@gmail.com>
2022-01-19 17:40:41 +0300

    added method bind; delete methods onClick and setItemClickListener

--- app/src/main/java/com/example/newsappnovosad/adapter/ListSourceViewHolder.kt
+++ app/src/main/java/com/example/newsappnovosad/adapter/ListSourceViewHolder.kt
@@ -1,22 +1,28 @@
 package com.example.newsappnovosad.adapter
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.newsappnovosad.R
-import com.example.newsappnovosad.service.ItemClickListener
+import com.example.newsappnovosad.model.Article
 
-class ListSourceViewHolder(itemView: View): RecyclerView.ViewHolder(itemView),View.OnClickListener {
-    var source_title = itemView.findViewById<RecyclerView>(R.id.id_TVNewsHeading) // внутри метода bind
-    private lateinit var itemClickListener:ItemClickListener
+class ListSourceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun setItemClickListener(itemClickListener: ItemClickListener){
-        this.itemClickListener = itemClickListener
+    private val title: TextView = itemView.findViewById(R.id.news_id_TVNewsHeading)
+    private val subTitle: TextView = itemView.findViewById(R.id.news_idSubTitle)
+    private val image: ImageView = itemView.findViewById(R.id.news_id_IVNews)
+
+    fun bind(article: Article, clickListener: () -> Unit) {
+        title.text = article.title
+        subTitle.text = article.description
+
+        Glide.with(image.context)
+            .load(article.urlToImage)
+            .into(image)
+        image.setOnClickListener {
+           clickListener()
+        }
     }
-
-    override fun onClick(p0: View?) {
-        itemClickListener.onCLick(p0!!,adapterPosition)
-    }
-
-
-
 }
